1bb7188d4099d74476ebe8baa39bb35e734203d7
Pablo Verly <pverly@redegazeta.com.br>
2023-07-05 18:49:30 -0300

    commit

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -8,7 +8,7 @@ namespace ApiTools.Repositories
         {
             var users = new List<User>();
             users.Add(new User { Id = 1, Username = "admin", Password = "admin", Role = "adm" });
-            users.Add(new User { Id = 2, Username = "robin", Password = "robin", Role = "user" });
+            users.Add(new User { Id = 2, Username = "pverly", Password = "pverly", Role = "user" });
             return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == password).FirstOrDefault();
         }
     }
